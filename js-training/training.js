5d6cb01746e61cb723ee8ae1e1bccee4204294f2
Maksimka66 <maxfese@gmail.com>
2023-11-19 02:25:07 +0200

    gfdgddg

---
+++ js-training/training.js
@@ -0,0 +1,8 @@
+class User {
+  constructor(name, email) {
+    console.log(name, email);
+  }
+}
+
+const mango = new User('Mango', 'mango@mail.com'); // "Mango mango@mail.com"
+console.log(mango); // {}
